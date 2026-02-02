ab221e8eac4eefce789e622ce2d6f4e493b8a855
xeisys <vbraeoficalah@gmail.com>
2026-02-02 14:22:43 +0400

    Initial project skeleton

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+import { Client, GatewayIntentBits } from 'discord.js';
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+    console.log('Bot is online!');
+});
+
+client.login(process.env.DISCORD_TOKEN);
